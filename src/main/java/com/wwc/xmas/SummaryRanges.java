4df0835037d8ab1702a68294b2e48c0e4a347d1d
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-12-02 23:20:19 +0000

    Update SummaryRanges.java
    Day 2 of coding challenge

--- src/main/java/com/wwc/xmas/SummaryRanges.java
+++ src/main/java/com/wwc/xmas/SummaryRanges.java
@@ -1,4 +1,31 @@
 package com.wwc.xmas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SummaryRanges {
+
+
+    public static void main(String[] s){
+        List<String> result = summaryRanges(new int[]{0,1,2,4,5,7});
+        System.out.println(result);
+    }
+    //Input: nums = [0,1,2,4,5,7]
+    //Output: ["0->2","4->5","7"]
+    public static List<String>  summaryRanges(int[] nums) {
+        List<String> ranges = new ArrayList<>();
+        int x=nums[0];
+        int y = 0;
+        for(int i=0; i<nums.length-1;i++){
+            //y=nums[i];
+            if ((i+1)== nums.length-1 && (nums[i]+1)!=nums[i+1] ){
+                ranges.add(x+"->"+nums[i]);
+                x=nums[i+1];
+            } else if ((nums[i]+1)!=nums[i+1] ){
+                ranges.add(x+"->"+nums[i]);
+                x=nums[i+1];
+            }
+        }
+        return ranges;
+    }
 }
