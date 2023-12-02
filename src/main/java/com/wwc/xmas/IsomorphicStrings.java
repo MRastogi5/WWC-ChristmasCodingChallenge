cd594841d6e7a47ec1ae4c4093153ef7ea68f04b
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-12-02 22:19:20 +0000

    Update IsomorphicStrings.java
    Day 1 coding challenge

--- src/main/java/com/wwc/xmas/IsomorphicStrings.java
+++ src/main/java/com/wwc/xmas/IsomorphicStrings.java
@@ -8,16 +8,13 @@ public class IsomorphicStrings {
     public boolean isIsomorphic(String s, String t) {
 
         Map<Character, Character> map = new HashMap<>();
-
         for (int i = 0; i < s.length(); i++) {
             char char_str1 = s.charAt(i), char_str2 = t.charAt(i);
-            if (map.containsKey(char_str1))
-            {
+            if (map.containsKey(char_str1)) {
                 if (map.get(char_str1) != char_str2)
                     return false;
             }
-            else
-            {
+            else {
                 if (map.containsValue(char_str2))
                     return false;
                 map.put(char_str1, char_str2);
