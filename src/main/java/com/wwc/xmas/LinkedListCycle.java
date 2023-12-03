c9b3158b0f991e2b1cff1eafabb70d03a3cb7575
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-12-03 21:11:58 +0000

    Create LinkedListCycle.java
    Day 3 of coding challenge: Leet code

---
+++ src/main/java/com/wwc/xmas/LinkedListCycle.java
@@ -0,0 +1,20 @@
+package com.wwc.xmas;
+
+// Day 3 of WWD- Christmas Coding Challenge
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+
+        return false;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
