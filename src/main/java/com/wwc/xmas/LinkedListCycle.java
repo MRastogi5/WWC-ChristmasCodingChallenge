8dbb4faea8e37f6fecf4c7d1e46821c46075f612
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-12-04 23:10:58 +0000

    Solution for LinkedList

--- src/main/java/com/wwc/xmas/LinkedListCycle.java
+++ src/main/java/com/wwc/xmas/LinkedListCycle.java
@@ -1,12 +1,39 @@
 package com.wwc.xmas;
 
 // Day 3 of WWD- Christmas Coding Challenge
+//https://leetcode.com/problems/linked-list-cycle/
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class LinkedListCycle {
 
     public boolean hasCycle(ListNode head) {
 
-        return false;
+
+        if(null == head || head.next == null){
+            return false;
+        }
+        boolean flag = false;
+        Set<ListNode> set = new HashSet<>();
+        set.add(head);
+        head = head.next;
+        while (head.next != null){
+            if (set.contains(head)) return true;
+            else set.add(head);
+            head = head.next;
+            flag = false;
+        }
+        return flag;
+    }
+
+    public static void main(String[] s ){
+
+        ListNode headlist = new ListNode(1);
+        headlist.next = new ListNode(2);
+        //headlist.next.next = new ListNode(3);
+        System.out.println(new LinkedListCycle().hasCycle(headlist));
+
     }
 }
 
