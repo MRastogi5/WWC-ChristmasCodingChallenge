84b9e196451f61e4935a81cc635826266d20f85a
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-12-04 20:46:18 +0000

    Day 4 of coding challenge

---
+++ src/main/java/com/wwc/xmas/AverageLevelsBinaryTree.java
@@ -0,0 +1,7 @@
+package com.wwc.xmas;
+
+
+// Day 4 of WWD- Christmas Coding Challenge
+//https://leetcode.com/problems/average-of-levels-in-binary-tree/
+public class AverageLevelsBinaryTree {
+}
--- src/main/java/com/wwc/xmas/SummaryRanges.java
+++ src/main/java/com/wwc/xmas/SummaryRanges.java
@@ -1,31 +1,53 @@
 package com.wwc.xmas;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+
+// Day 2 of WWD- Christmas Coding Challenge
+// https://leetcode.com/problems/summary-ranges/
 
 public class SummaryRanges {
-
-
     public static void main(String[] s){
         List<String> result = summaryRanges(new int[]{0,1,2,4,5,7});
-        System.out.println(result);
+        //List<String> result = summaryRanges(new int[]{0,2,3,4,6,7,8});
+        //List<String> result = summaryRanges(new int[]{0,2,3,4,6,8,9});
+        // 123456
+        //System.out.println(summaryRanges(new int[]{0,1,2,4,5,7}));
+        //System.out.println(summaryRanges(new int[]{0,2,3,4,6,7,8}));
+        System.out.println(summaryRanges(new int[]{0,1,2,4,5,7,9,10,12,14,15,16,27,28}));
+        //System.out.println(result);
     }
-    //Input: nums = [0,1,2,4,5,7]
-    //Output: ["0->2","4->5","7"]
     public static List<String>  summaryRanges(int[] nums) {
-        List<String> ranges = new ArrayList<>();
-        int x=nums[0];
-        int y = 0;
-        for(int i=0; i<nums.length-1;i++){
-            //y=nums[i];
-            if ((i+1)== nums.length-1 && (nums[i]+1)!=nums[i+1] ){
-                ranges.add(x+"->"+nums[i]);
-                x=nums[i+1];
-            } else if ((nums[i]+1)!=nums[i+1] ){
-                ranges.add(x+"->"+nums[i]);
-                x=nums[i+1];
+
+        if(nums.length<=0){
+            return new ArrayList<>();
+        }
+        List<String> rangeArr = new ArrayList<>();
+        Map<Integer, Integer> values = new LinkedHashMap();
+        int x = nums[0];
+        values.put(x, nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            int current = nums[i];
+            if (nums[i - 1] + 1 == current) {
+                if (i + 1 == nums.length) {
+                    values.put(x, current);
+                }
+                continue;
+            } else if (nums[i - 1] + 1 != current) {
+                values.put(x, nums[i - 1]);
+                x = current;
+                if (i + 1 == nums.length) {
+                    values.put(x, current);
+                }
             }
         }
-        return ranges;
+
+        for (int i : values.keySet()) {
+            int j = values.get(i);
+            if (i == j) {
+                rangeArr.add(i + "");
+            } else rangeArr.add(i + "->" + j);
+        }
+        return rangeArr;
     }
 }
